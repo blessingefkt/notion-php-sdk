c7b3b94a32a6960858fadd3d67271f44d8768fa5
Blessing Richardson <blessing@sayla.co>
2021-12-01 04:01:41 -0600

    fix

--- src/Notion/Properties/Number.php
+++ src/Notion/Properties/Number.php
@@ -6,4 +6,13 @@ use Notion\Traits\HasSimpleValue;
 class Number extends PropertyBase
 {
     use HasSimpleValue;
+
+    public function set($value): void
+    {
+        if (is_string($value)) {
+            $value = floatval($value);
+        }
+        $this->config->{$this->config->type} = $value;
+    }
+
 }
